ccf9bd862489ab2bbdc7105fdfd05a56f7586246
tuk86214 <tuk86214@temple.edu>
2024-10-30 15:34:52 -0400

    Implement fragment loading based on orientation

--- app/src/main/java/edu/temple/dicethrow/MainActivity.kt
+++ app/src/main/java/edu/temple/dicethrow/MainActivity.kt
@@ -19,6 +19,8 @@ The Activity layout files for both Portrait and Landscape are already provided
 */
 
 class MainActivity : AppCompatActivity(), ButtonFragment.ButtonInterface {
+    private lateinit var buttonFragment: ButtonFragment
+    private lateinit var dieFragment: DieFragment
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -27,14 +29,34 @@ class MainActivity : AppCompatActivity(), ButtonFragment.ButtonInterface {
             - Show only Button Fragment if portrait
             - show both fragments if Landscape
           */
+
+        buttonFragment = ButtonFragment()
+        dieFragment = DieFragment()
+
+        if (resources.configuration.orientation == android.content.res.Configuration.ORIENTATION_LANDSCAPE) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container1, buttonFragment)
+                .commit()
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container2, dieFragment)
+                .commit()
+        } else {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container1, buttonFragment)
+                .commit()
+        }
     }
 
     /* TODO 2: switch fragments if portrait (no need to switch fragments if Landscape)
         */
     // Remember to place Fragment transactions on BackStack so then can be reversed
     override fun buttonClicked() {
+        if (resources.configuration.orientation == android.content.res.Configuration.ORIENTATION_PORTRAIT) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container1, dieFragment)
+                .addToBackStack(null)
+                .commit()
 
+        }
     }
-
-
 }
